fe5e1b647451945a50d94042980f844d46724900
Yogesh <yogesh@logiceasetecno.com>
2026-05-20 13:15:44 +0530

    rename Index.tsx to index.tsx

---
+++ app/no_Tabs/ViewCart/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ViewCart = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          textAlign: "center",
+          backgroundColor: "red",
+        }}
+      >
+        index
+      </Text>
+    </View>
+  );
+};
+
+export default ViewCart;
+
+const styles = StyleSheet.create({});
